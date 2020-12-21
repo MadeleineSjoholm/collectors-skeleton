16f74d410ef5dbe4b4ba29219f6a7baee102ae53
FeliciaOstling <felicia.ostling@hotmail.com>
2020-12-21 17:18:45 +0100

    nya knapp

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    Rules
+
+    <div class="infoButtonn" id="infoo">
+        <router-link :to="{ name: 'Home', params: {} }"><img src="/images/pil.png" width= "60" ></router-link> Back to homepage
+
+
+
+
+    </div>
+
   </div>
+
   </template>
 
   <script>
@@ -14,6 +23,16 @@
   }
   </script>
   <style>
+
+  .infoButtonn{
+    position: absolute;
+    margin-top: 15px;
+    margin-left: 15px;
+
+  }
+  #infoo{
+    width: 50px;
+  }
 
 
 
